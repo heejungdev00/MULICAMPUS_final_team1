7d2175f831251e020c25d621f5552a9d531ad207
github jecdev0612 <github deveunchae0612@gmail.com>
2024-07-16 17:49:25 +0900

    gpt

---
+++ jec/gpt.py
@@ -0,0 +1,44 @@
+def get_angles(landmarks):
+    angles = []
+    
+    # 오른쪽 팔꿈치 각도
+    right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                      landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                   landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                   landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+    angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+    angles.append(angle)
+    
+    # 왼쪽 팔꿈치 각도
+    left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                  landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+    left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                  landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+    angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+    angles.append(angle)
+    
+    # 오른쪽 무릎 각도
+    right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                 landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+    right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                  landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+    right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                   landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+    angle = calculate_angle(right_hip, right_knee, right_ankle)
+    angles.append(angle)
+    
+    # 왼쪽 무릎 각도
+    left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                 landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+    left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                  landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+    angle = calculate_angle(left_hip, left_knee, left_ankle)
+    angles.append(angle)
+    
+    return angles
